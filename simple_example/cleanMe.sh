a88be0aa3ce2c9aec98185a2267935dd9538b888
Brian Haas <brianjohnhaas@gmail.com>
2023-01-16 14:53:28 -0500

    add test cleaner

---
+++ simple_example/cleanMe.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+rm -rf ./__smalltest* ./smalltest.*
+
+
--- simple_example/runMe.sh
+++ simple_example/runMe.sh
@@ -1,30 +1,18 @@
 #!/bin/sh
 
+set -ex
 
 ## run EVM
 
-../evidence_modeler.pl --genome genome.fasta \
-                       --weights ./weights.txt \
-                       --gene_predictions gene_predictions.gff3 \
-                       --protein_alignments protein_alignments.gff3 \
-                       --transcript_alignments transcript_alignments.gff3 \
-                     > evm.out 
+../EVidenceModeler --sample_id smalltest \
+                   --genome genome.fasta \
+                   --weights ./weights.txt \
+                   --gene_predictions gene_predictions.gff3 \
+                   --protein_alignments protein_alignments.gff3 \
+                   --transcript_alignments transcript_alignments.gff3 \
+                   --segmentSize 100000 \
+                   --overlapSize 10000
 
-echo
-echo
-echo "*** Created EVM output file: evm.out ***"
-
-
-## convert output to GFF3 format
-./../EvmUtils/EVM_to_GFF3.pl evm.out.orig Contig1 > evm.out.gff3
-
-echo
-echo
-echo "*** Converted EVM output to GFF3 format: evm.out.gff3 ***"
-
-echo
-echo "Done."
-
-
-
+      
+echo "Done. See smalltest.EVM.* outputs"
 
